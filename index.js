b779c7bccfe7d8fe09b0b2fed30f5cf72a242e61
Phinn Sriployrung <67180840+sriphinn@users.noreply.github.com>
2020-11-11 08:56:48 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+
+const apiKey = 'NseftDiMJi8MAGYlzITQbCrWr5ZDpWNYGDYswbgg'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+};
+
+function getNationalParks(query, maxResults=10) {
+  const params = {
+    stateCode: query,
+    limit: maxResults,
+    api_key: apiKey
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+  
+  console.log(url);
+
+  fetch(url)
+    .then (response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+        $('#results').removeClass('hidden');
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+};
+
+function displayResults(responseJson) {
+    console.log(responseJson)
+    $('#results-list').empty();
+    $('#results').removeClass('hidden');
+    for (let i = 0; i < responseJson.limit; i++)
+        $('#results-list').append(
+            `<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+            <p>${responseJson.data[i].addresses[0].line1}, ${responseJson.data[i].addresses[0].city}, ${responseJson.data[i].addresses[0].stateCode}, ${responseJson.data[i].addresses[0].postalCode}</p>
+            <img src="${responseJson.data[i].images[0].url}">
+            <p>${responseJson.data[i].description}</p>`
+        )
+}
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        //need to grab the value of up to both state selections and make them a single string seperated by comma//
+        let stateOne = $('#state-one').val(); 
+        const stateTwo = $('#state-two').val(); 
+        const maxResults = $('#js-max-results').val();
+        if (stateTwo) {
+            stateOne += ',' + stateTwo
+        }
+    
+        console.log(maxResults);
+        console.log(stateOne);
+        getNationalParks(stateOne, maxResults);
+    });
+};
+
+$(function() {
+    console.log("App loaded! Waiting for user input and submit.");
+    watchForm();
+});
